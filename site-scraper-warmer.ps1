8a73a0bc0b7f5bfcab02ff5b3711750b01bf861d
Leo <leonardjonathanoh@live.com>
2017-03-09 03:46:13 +0800

    fix bugs, new features, optimized script

--- site-scraper-warmer.ps1
+++ site-scraper-warmer.ps1
@@ -18,25 +18,21 @@ $main_sitemap_uri = "sitemap.xml"
 # Default: "$desired_protocol$domain/$main_sitemap_uri"
 $sitemap = "$desired_protocol$domain/$main_sitemap_uri"
 
+# tag-attribute combinations where uris should be searched (Comma-delimited, order doesn't matter)
+# NOTE: best left in their default values.
+# NOTE: if you would like to add another combination, append it to the string (ensure you add a comma before each entry)
+$tag_attribute_combos = "a href, img src, img data-src, img srcset, img data-srcset, link href, script src, img data-noscript, ...custom "
+
 # data files and directories
 # NOTE: best left in their default values.
 $sitemaps_file = "sitemaps.txt"
 $links_file = "links.txt"
 $html_dir = "html"
 $uri_sets_dir = "uri_sets"
-$a_href_file = "a_href.txt"
-$img_src_file = "img_src.txt"
-$img_srcset_file = "img_srcset.txt"
-$link_href_file = "link_href.txt"
-$script_src_file = "script_src.txt"
+$uri_sets_file_extension = ".txt"
 $curls_dir = "curls"
 $curls_sitemaps_file = "sitemaps"
 $curls_links_file = "links"
-$curls_a_href_file = "a_href"
-$curls_img_src_file = "img_src"
-$curls_img_srcset_file = "img_srcset"
-$curls_link_href_file = "link_href"
-$curls_script_src_file = "script_src"
 
 # whether the script should stop at just retreiving sitemaps links, or continue to get all uris from all those links
 # 0 - retrieve links and continue to get all their uris
@@ -90,18 +86,20 @@ function uri_is_valid ([string]$str) {
  # Param 1: html string
  # Param 2: html tag e.g. img
  # Param 3: html tag's attribute e.g. src
- # Param 4: array to store uris
+ # Param 4: [System.Collections.ArrayList] arraylist to store uris
 #
-function get_uris ([string]$html_str, [string]$tag, [string]$attr, [array]$array) {
+function get_uris ([string]$html_str, [string]$tag, [string]$attr, [System.Collections.ArrayList]$arraylist) {
     # split html by html tag opening char i.e. <
     $html_arr = $html_str.split('<')
-    
+
     # for each line with tag of interest found, do
-    $html_arr | where { $_ -match "^$tag "} | foreach { # <img 
+    $html_arr | where { $_ -match "^$tag "} | foreach { # e.g. <img 
         # capture attribute's value
-        $captures = [regex]::Match( $_, " $attr=(?:`"([^`"]*)`"|'([^']*)')" ) # src="(https://theohbrothers.com/)"
+        $attr_regex = $attr.replace('.', '\.').replace('-', '\-') 
+        $regex = " $attr_regex=(?:`"([^`"]*)`"|'([^']*)')" # e.g. data\-src="(https://theohbrothers.com/)"
+        $captures = [regex]::Match( $_, $regex ) 
         $attr_val = if($captures.Groups[1].value -ne '') {$captures.Groups[1].value} else {$captures.Groups[2].value}
-        if($debug) { write-host "$_`n : $($captures.Groups[1].value -eq '') vs $($captures.Groups[2].value -eq '') `n 1: $($captures.Groups[1].value)`n 2: $($captures.Groups[2].value)"; }
+        if($debug) { write-host "`n$_`nRegex:$regex`n Group 1 (double-quotes) found: $($captures.Groups[1].value -eq ''), Group 2 (single-quotes) found: $($captures.Groups[2].value -eq '') `n 1: $($captures.Groups[1].value)`n 2: $($captures.Groups[2].value)"; }
 
         # in the case of comma-delimited values e.g. <img srcset>, split values
         $attr_vals = $attr_val.Split(',') # for <img srcset="http://tob.com/1.jpg 150w, http://tob.com/2.jpg 250w, ..."
@@ -114,55 +112,54 @@ function get_uris ([string]$html_str, [string]$tag, [string]$attr, [array]$array
 
             # filter uris we want
 			if (uri_is_valid ($val)) {
-				if (!$array.Contains($val)) {
-					$array += $val
+				if (!$arraylist.Contains($val)) {
+					$null = $arraylist.Add($val) # assigning it to $null removes arraylist's Add()'s return value.
 				}
 			}
         } 
     }
-    return $array
+    #return $array
 }
 
 # replaces protocol with our desired
-function replace_protocol([array]$array) {
+ # Param 1: array or System.Collections.ArrayList
+function replace_protocol($array) {
 	#Write-Host $array.Count
 	for($i=0; $i -lt $array.count; $i++) {
 		$uri = $array[$i]
-		$captures = [regex]::Match( $uri, '^((?:https?:)?\/\/)' ) # capture protocol part including the //
-		$prot = $captures.Groups[0].Value
-		$array[$i] = $uri -replace $prot, $desired_protocol
-
+		$matches = [regex]::Match( $uri, '^((?:https?:)?\/\/)' ) # capture protocol part including the //
+		$uri_protocol = $matches.Groups[1].Value
+		$array[$i] = $uri -replace $uri_protocol, $desired_protocol
 	}
 	<# using foreach from: http://stackoverflow.com/questions/34166023/powershell-modify-elements-of-array
 	$array = $array | foreach {
-		$captures = [regex]::Match( $_, '((?:https?:)?\/\/)' )
-		$prot = $captures.Groups[0].Value
-		$new_uri = $_ -replace $prot, 'http://'
+		$matches = [regex]::Match( $_, '((?:https?:)?\/\/)' )
+		$uri_protocol = $matches.Groups[1].Value
+		$new_uri = $_ -replace $uri_protocol, 'http://'
 		$_ = $new_uri
 		$_
 	}#>
 }
 # ouputs curls to a file
- # Param 1: hashtable of format: uri_set_array => uri_file_string
+ # Param 1: # hashtable of format: [System.Collections.ArrayList] uri_set => [string] uri_set_filename
  # Param 2: directory to write curl files 
  # Param 3: OS that curls will run on. 0: *nix; 1: WinNT
 #
 function output_curls($hashtable, [string]$dir, [int]$OS) {
     # create directory to store curls, if not existing
-    if (!(Test-Path $dir)) {New-Item -ItemType directory $dir}
-    $comment_char = if($OS -eq 1) { "::" } else { "#" }
-    $toNull = if($OS -eq 1) { " > NUL" } else { " > /dev/null " }
-    $ext = if($OS -eq 1) { ".bat" } else { ".sh" }
+    if (!(Test-Path $dir)) {$null = New-Item -ItemType directory $dir} # Assigning it to $null removes the return value
+    $commentChar = if($OS -eq 1) { "::" } else { "#" }
+    $toNull = if($OS -eq 1) { " >NUL" } else { " > /dev/null " }
+    $extension = if($OS -eq 1) { ".bat" } else { ".sh" }
     $hashtable.GetEnumerator() | % { 
-        $curls = @("$comment_char $(Get-Date)", "$comment_char -k to ignore ssl cert")
+        $curls = @("$commentChar $(Get-Date)", "$commentChar -k to ignore ssl cert")
         $uri_set = $_.key
         $uri_set_curls_file = $_.value
-        foreach ($l in $uri_set) {   
-            
-	        $curls += "curl -k -X GET $l $toNull"
+        foreach ($uri in $uri_set) {   
+	        $curls += "curl -k -X GET $uri $toNull"
         }
-        $curls | Out-File "$dir/$uri_set_curls_file$ext" -Encoding utf8
-        Write-Host "> $($uri_set.count) curls in $dir/$uri_set_curls_file$ext" -ForegroundColor Green
+        $curls | Out-File "$dir/$uri_set_curls_file$extension" -Encoding utf8
+        Write-Host "> $($uri_set.count) curls in $dir/$uri_set_curls_file$extension" -ForegroundColor Green
     } 
 }
 #########################################
@@ -226,39 +223,88 @@ Write-Host "> $($links.count) links in $links_file" -ForegroundColor Green
 Write-Host "`n`n[Writing curls for sitemaps and links...]" -ForegroundColor Cyan
 
 # output curls of sitemap and links to files
-$hashtable0 = [ordered]@{$sitemaps = $curls_sitemaps_file
-                $links = $curls_links_file}
+# edit: 2017 March - not using constructor anymore. Hashtable will NULL if any key is empty.)
+<#$hashtable0 = [ordered]@{$sitemaps = $curls_sitemaps_file
+                         $links = $curls_links_file}#>
+$hashtable0 = [ordered]@{}
+    $hashtable0.Add($sitemaps, $curls_sitemaps_file)
+    $hashtable0.Add($links, $curls_links_file)
 output_curls $hashtable0 $curls_dir $OS_WinNT
 
 # continue further only if user wants to
 if($mode_sitemap_links_only -eq 1) { pause; exit }
 
-# tell user we are going to scrape all links for uri sets
-Write-Host "`n`n[Scraping links to get uri sets ...]" -ForegroundColor Cyan
+# determine uri_sets that should be search for
+$uri_sets = [ordered]@{} # hashtable: [string]$tag => [array]$attributes
+foreach ($combo in $tag_attribute_combos) {
+    $split = $combo.split( @(',', ' ') ).trim() | ? {$_} # exclude empty values. 
+   
+    for($i=0; $i -lt $split.count; $i++) {
+        # skip over odd numbers
+        if($i % 2) { continue } 
 
-# scrape all links from our site for all uri sets
+        $tag  = $split[$i]
+        $attr = $split[$i+1]
+
+        
+        # skip over invalid tags / attributes (may contain letters and dashes only)
+        if($tag -match [regex]"^[A-Za-z\-]+" -eq $false -or $tag -match [regex]"^[A-Za-z\-]+" -eq $false) { continue }
+
+        if($uri_sets.Contains($tag) -eq $false) {
+            # first tag of its kind found, add the combination
+            $uri_sets.Add($tag, [array]$attr) # e.g. 'a' => @('href'), e.g. 'img' => @('src')
+        }
+        else
+        {
+            # append more attributes for an existing tag
+            $attrs = $uri_sets.($tag) # retrieve the existing attributes
+            if($attrs.Contains($attr) -eq $false) {
+                $attrs += $attr    # add new attribute for this tag
+                $uri_sets.($tag) = $attrs #e.g. 'img' => @('src', 'srcset')
+            }
+        }
+    }
+}
+
+
+# show the user the uri sets we will search for
+Write-Host "`n`n[Uri sets to search for]" -ForegroundColor Cyan
+$uri_sets 
+
+# get all links of our site to scrape 
 $links_to_scrape = Get-Content -Path $links_file -Encoding utf8
-$a_href_all = @()
-$link_href_all = @()
-$img_src_all = @()
-$img_srcset_all = @()
-$script_src_all = @()
-$i = 0; # number of links scraped
+
+# scrape count
+$i = 0;
+
+# declare/reset the uri set variables e.g. $a_href_all, $img_src_all, $img_data-src_all, ...
+$uri_sets.GetEnumerator() | % {
+    $tag = $_.key
+    $attrs = $_.value
+    foreach ($attr in $attrs) {
+        $new = New-Object System.Collections.ArrayList
+        Set-Variable -Name "$($tag)_$($attr)_all" -Value $new
+    }
+}
+# tell user we are going to scrape all site's links for uri sets
+Write-Host "`n`n[Scraping site's links to get desired
+ uri sets ...]" -ForegroundColor Cyan
 
 # create directory to store .html, if not existing
-if (!(Test-Path $html_dir)) {New-Item -ItemType directory $html_dir} # create html folder if not existing
+if (!(Test-Path $html_dir)) {$null = New-Item -ItemType directory $html_dir; }  # Assigning it to $null removes the return value
 
 # scrape links and parse .html to get uri sets: <a href>, <img src>, <img srcset>, <link href>, <script src>
 foreach ($l in $links_to_scrape) {
 	$i++
 	# Scrape, while warming the link
-	$html = Invoke-WebRequest -uri $l #-UseBasicParsing
+	$html = Invoke-WebRequest -uri $l -UseBasicParsing
     
 	# output html to file
 	$html.Content | Out-File "$html_dir/$i.html" -Encoding utf8
 
 	# parse html to get uri sets: <a href>, <img src>, <img srcset>, <link href>, <script src>
-	$html.links | foreach {
+    # edit 2017 March - not using engine to parse html anymore
+	<#$html.links | foreach {
 		$val = $_.href
 		if (uri_is_valid($val)) {
 			if (!$a_href_all.Contains($val)) {
@@ -273,21 +319,63 @@ foreach ($l in $links_to_scrape) {
 				$img_src_all += $val
 			}
 		}
-	}
+	}#>
     # use manual parsing of offline html for other uri sets (works on *nix without IE's parsing)
     $html = Get-Content "$html_dir/$i.html" -Raw -Encoding utf8
-    $img_srcset_all = get_uris $html 'img' 'srcset' $img_srcset_all 
-    $link_href_all = get_uris $html 'link' 'href' $link_href_all
-    $script_src_all = get_uris $html 'script' 'src' $script_src_all
+    $uri_sets.GetEnumerator() | % {
+        $tag = $_.key
+        $attrs = $_.value
+        foreach ($attr in $attrs) {
+            $uri_set = Get-Variable -Name "$($tag)_$($attr)_all" -ValueOnly # returns null if variable doesn't exist, or else returns an arraylist
+            get_uris $html $tag $attr $uri_set         # e.g. $uri_set = get_uris $html 'a' 'href' $uri_set_all 
+            # declare new variable with value
+            Set-Variable -Name "$($tag)_$($attr)_all" -Value $uri_set  # e.g. $a_href_all = $uri_set, e.g. $img_src_all = $uri_set
+            if($debug) { "$tag $attr $($tag)_$($attr)_all" }
+        }
+    }
 }
-Write-Host "> Successfully retrieved all uri sets." -ForegroundColor Green
-# map uri sets to files
-$hashtable1=''
-$hashtable1=[ordered]@{ $a_href_all = $a_href_file;
-						$img_src_all = $img_src_file;
-						$img_srcset_all = $img_srcset_file;
-						$link_href_all = $link_href_file; 
-						$script_src_all = $script_src_file ;} #hashtable: uri_set_array => uri_file_string
+# tell user we successfully retrieved all uri sets from our site's links
+Write-Host "> Successfully retrieved all uri sets from site's links." -ForegroundColor Green
+
+# debug - any empty uri sets?
+if ($debug) { 
+    Write-Host "`n`n[Debug - Checking for any empty uri sets ...]" -ForegroundColor Cyan
+    $uri_sets.GetEnumerator() | % {
+        $tag = $_.key
+        $attrs = $_.value
+        foreach ($attr in $attrs) {
+            if( (Get-Variable -Name "$($tag)_$($attr)_all" -ValueOnly) -eq $NULL) { 
+                Write-Host "$($tag)_$($attr)_all set is empty" -ForegroundColor Gray  
+            }
+        }
+        
+    }
+}
+
+# map uri sets to files 
+# edit: 2017 March - not using constructor anymore. Hashtable will NULL if any key is empty.)
+# using .Add() will not add a key-value pair if the key is empty.
+$hashtable1 = [ordered]@{} # hashtable: [System.Collections.ArrayList] uri_set => [string] uri_set_filename_without_extension
+$uri_sets.GetEnumerator() | % {
+    $tag = $_.key
+    $attrs = $_.value
+    foreach ($attr in $attrs) {
+        $uri_set = Get-Variable -Name "$($tag)_$($attr)_all" -ValueOnly 
+        if($uri_set.count -eq 0) { $null = $uri_set.Add('') } # if uri_set is null, give it an empty value anyway. Assigning it to $null removes arraylist's Add()'s return value.
+        $uri_set_filename = "$($tag)_$($attr)" # e.g. a_href
+        $hashtable1.Add($uri_set, $uri_set_filename);
+    }  
+}
+
+# debug - show individual uri sets' contents
+if ($debug) { 
+    Write-Host "`n`n[Debug - Showing individual uri set's content ...]" -ForegroundColor Cyan
+    $hashtable1.GetEnumerator() | % {
+        Write-Host "`n ---- $($_.value) ----"
+        $_.key
+    }
+}
+
 # replace protocol with our desired for all uris
 $hashtable1.GetEnumerator() | % { 
    replace_protocol ($_.key)
@@ -297,14 +385,14 @@ $hashtable1.GetEnumerator() | % {
 Write-Host "`n`n[Writing all uri sets to their files...]" -ForegroundColor Cyan
 
 # create directory to store uri_sets, if not existing
-if (!(Test-Path $uri_sets_dir)) {New-Item -ItemType directory $uri_sets_dir}
+if (!(Test-Path $uri_sets_dir)) {$null = New-Item -ItemType directory $uri_sets_dir}  # Assigning it to $null removes the return value
 
 # output uri sets to files
 $hashtable1.GetEnumerator() | % { 
    $uri_set = $_.key
-   $uri_set_file = $_.value
-   $uri_set | Out-File "$uri_sets_dir/$uri_set_file" -Encoding utf8
-   Write-Host "> $($uri_set.count) uris in $uri_sets_dir/$uri_set_file" -ForegroundColor Green
+   $uri_set_filename = $_.value
+   $uri_set | Out-File "$uri_sets_dir/$uri_set_filename$uri_sets_file_extension" -Encoding utf8
+   Write-Host "> $($uri_set.count) uris in $uri_sets_dir/$uri_set_filename$uri_sets_file_extension" -ForegroundColor Green
 }
 
 # tell user we successfully wrote all uri sets to their files
@@ -314,13 +402,7 @@ Write-Host "`n> Successfully output all uri sets to their files." -ForegroundCol
 Write-Host "`n`n[Writing curls for all uri sets to their files...]" -ForegroundColor Cyan
 
 # output curls uri sets to files
-$hashtable2=''
-$hashtable2=[ordered]@{ $a_href_all = $curls_a_href_file;
-                        $img_src_all = $curls_img_src_file;
-                        $img_srcset_all = $curls_img_srcset_file;
-                        $link_href_all = $curls_link_href_file; 
-                        $script_src_all = $curls_script_src_file ;} #hashtable: uri_set_array => uri_file_string
-output_curls $hashtable2 $curls_dir $OS_WinNT
+output_curls $hashtable1 $curls_dir $OS_WinNT
 
 # tell user we successfully wrote curls commands for all uri sets
 Write-Host "`n> Successfully wrote curls commands for all uri sets." -ForegroundColor Green

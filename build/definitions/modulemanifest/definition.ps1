4ae7f92aba838c01b5c4c97efbf3e5a7a82384cd
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-28 00:41:16 +0000

    Enhancement (ci): Add PSModulePublisher submodule
    This adds module manifest definition file and generates the module manifest

---
+++ build/definitions/modulemanifest/definition.ps1
@@ -0,0 +1,59 @@
+# - Initial setup: Fill in the GUID value. Generate one by running the command 'New-GUID'. Then fill in all relevant details.
+# - Ensure all relevant details are updated prior to publishing each version of the module.
+# - To simulate generation of the manifest based on this definition, run the included development entrypoint script Invoke-PSModulePublisher.ps1.
+# - To publish the module, tag the associated commit and push the tag.
+@{
+    RootModule = 'PSSiteScraper.psm1'
+    # ModuleVersion = ''                            # Value will be set for each publication based on the tag ref. Defaults to '0.0.0' in development environments and regular CI builds
+    GUID = '7c956939-46c0-4866-bbe1-e800a2bc3cdc'
+    Author = 'The Oh Brothers'
+    CompanyName = 'The Oh Brothers'
+    Copyright = '(c) 2021 The Oh Brothers'
+    Description = 'Cmdlets for scraping a site.'
+    PowerShellVersion = '3.0'
+    # PowerShellHostName = ''
+    # PowerShellHostVersion = ''
+    # DotNetFrameworkVersion = ''
+    # CLRVersion = ''
+    # ProcessorArchitecture = ''
+    # RequiredModules = @()
+    # RequiredAssemblies = @()
+    # ScriptsToProcess = @()
+    # TypesToProcess = @()
+    # FormatsToProcess = @()
+    # NestedModules = @()
+    FunctionsToExport = @()
+    CmdletsToExport = @(
+        Get-ChildItem $PSScriptRoot/../../../src/PSSiteScraper/public -Exclude *.Tests.ps1 | % { $_.BaseName }
+    )
+    VariablesToExport = @()
+    AliasesToExport = @()
+    # DscResourcesToExport = @()
+    # ModuleList = @()
+    # FileList = @()
+    PrivateData = @{
+        # PSData = @{           # Properties within PSData will be correctly added to the manifest via Update-ModuleManifest without the PSData key. Leave the key commented out.
+            Tags = @(
+                'html'
+                'powershell'
+                'pwsh'
+                'scrape'
+                'site'
+                'sitemap'
+                'uri-scheme'
+                'uri'
+                'url'
+                'website'
+            )
+            LicenseUri = 'https://raw.githubusercontent.com/theohbrothersPSSiteScraper/master/LICENSE'
+            ProjectUri = 'https://github.com/theohbrothersPSSiteScraper'
+            # IconUri = ''
+            # ReleaseNotes = ''
+            # Prerelease = ''
+            # RequireLicenseAcceptance = $false
+            # ExternalModuleDependencies = @()
+        # }
+        # HelpInfoURI = ''
+        # DefaultCommandPrefix = ''
+    }
+}
